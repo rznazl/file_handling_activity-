bbcdf3d753f924bc44159810e54aa1803d4b25ff
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-04-27 22:21:55 +0800

    this is the part where the program reads the raw text. this also clears up any hidden spaces. it also converts the entered strings into an integer

--- square_and_cube_of_integers.py
+++ square_and_cube_of_integers.py
@@ -1,7 +1,7 @@
 class IntegerProcessor:
     def __init__(self, source_file):
         self.source_file = source_file
-        self.number = []
+        self.numbers = []
 
     def create_initial_file(self):
         sample_integers = [
@@ -11,4 +11,11 @@ class IntegerProcessor:
         with open(self.source_file, 'w') as file:
             file.write("\n".join(sample_integers))
         print(f"System: Created '{self.source_file}' with 20 integers.\n")
-        
+
+    def load_data(self):
+        try:
+            with open(self.source_file, 'r') as file:
+                self.numbers = [int(line.strip()) for line in file if line.strip()]
+        except FileNotFoundError:
+            self.create_initial_file()
+            self.load_data()
