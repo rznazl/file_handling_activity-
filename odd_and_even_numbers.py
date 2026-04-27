e3179ba631495eee1868f0d8abde9981e930e5fb
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-04-27 19:26:12 +0800

    Opens the file in read mode. It takes all the text and breaks it into individual strings. It takes each string and turns it into an integer.

--- odd_and_even_numbers.py
+++ odd_and_even_numbers.py
@@ -10,4 +10,14 @@ class NumberSorter:
         ]
         with open(self.input_file_path, 'w') as new_file:
             new_file.write("\n".join(sample_numbers))
-        print(f"File '{self.input_file_path}') created with 20 integers.")
+        (print(f"File '{self.input_file_path}') created with 20 integers."))
+
+    def load_numbers_from_file(self):
+        try:
+            with open(self.input_file_path, 'r') as source_file:
+                raw_data = source_file.read().split()
+                self.number_list = [int(item) for item in raw_data]
+        except FileNotFoundError:
+            print(f"Error: {self.input_file_path} not found. Now creating...")
+            self.create_sample_file()
+            self.load_numbers_from_file()
