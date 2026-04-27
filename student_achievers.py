0f8dff4356161c45b612a1a1134c5d232139dc20
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-04-27 20:10:48 +0800

    this is where the program stores the student's name and their gwa's. it converts the student's gwa into a float. it also shows the specific format of the information when printed

---
+++ student_achievers.py
@@ -0,0 +1,7 @@
+class StudentRecord:
+    def __init__(self, student_name, student_gwa):
+        self.student_name = student_name
+        self.student_gwa = float(student_gwa)
+
+    def __str__(self):
+        return f"Student: {self.student_name} | GWA: {self.student_gwa}"
