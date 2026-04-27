740276c372ace2b2d577707b63670ef1290d2470
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-04-27 21:12:27 +0800

    this is the part where the program asks the user to input any line. whatever is typed will be added to the list. tp break the loop, the user needs to enter the word no

--- life_diary.py
+++ life_diary.py
@@ -2,3 +2,12 @@ class Life_Happenings:
     def __init__(self, file_name):
         self.file_name = file_name
         self.lines_to_save = []
+
+    def collect_and_save(self):
+        while True:
+            line_input = input("Please enter anything about your life right now: ")
+            self.lines_to_save.append(line_input)
+
+            choice = input("Do you anything more to say? ").strip().lower()
+            if choice == 'no':
+                break
