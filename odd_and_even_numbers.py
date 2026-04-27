a3673697fe1655cfdf17d230175102aeee7788c2
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-04-27 19:20:36 +0800

    Create a file named numbers.txt and fill it with 20 numbers given

--- odd_and_even_numbers.py
+++ odd_and_even_numbers.py
@@ -2,3 +2,12 @@ class NumberSorter:
     def __init__(self, input_file_path):
         self.input_file_path = input_file_path
         self.number_list = []
+
+    def create_sample_file(self):
+        sample_numbers = [
+            "9", "28", "4", "66", "12", "89", "34", "7", "19", "11",
+            "92", "5", "18", "5", "77", "42", "29", "60", "81", "14"
+        ]
+        with open(self.input_file_path, 'w') as new_file:
+            new_file.write("\n".join(sample_numbers))
+        print(f"File '{self.input_file_path}') created with 20 integers.")
