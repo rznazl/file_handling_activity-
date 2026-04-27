3360ab0657d75825edce510b072b6edab0fcdf30
Rexine Aizelle A. Dimaranan <dimarananrexine@gmail.com>
2026-04-27 20:20:42 +0800

    It is the file that holds the list of all students

--- student_achievers.py
+++ student_achievers.py
@@ -5,3 +5,8 @@ class StudentRecord:
 
     def __str__(self):
         return f"Student: {self.student_name} | GWA: {self.student_gwa}"
+
+class GradeAnalyzer:
+    def __init__(self, data_file_path):
+        self.data_file_path = data_file_path
+        self.all_students = []
